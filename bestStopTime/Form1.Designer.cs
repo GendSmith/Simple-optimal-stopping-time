0d7ecc92c952d00ebe940afa33693a76dc748d86
GendSmith <1439786496@qq.com>
2018-06-09 22:35:55 +0800

    上传项目

---
+++ bestStopTime/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace bestStopTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 468);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "下一局";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button6666_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "当前局收益：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "总收益(已算上成本和折现)：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "下一局的期望(扣除成本和折现)：";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "每一局的成本：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "折现率：";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 404);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "策略提示：";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(173, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 15);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "随机游戏";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 255);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 207);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(285, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(285, 124);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(15, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(285, 170);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(15, 15);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "0";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(98, 404);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(127, 15);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "第一局免费试玩！";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(41, 308);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(370, 15);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "游戏规则：按下【下一局】按钮后就会在[1,12]区间内";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(44, 323);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(380, 15);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "随机选一个整数k，然后k*100是收益，但是如果k==12，";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(41, 338);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(382, 15);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "那么之前所有的收益将会被清零，只要玩家能够支付得起";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(41, 353);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(352, 15);
+            this.label15.TabIndex = 18;
+            this.label15.Text = "下一局的成本，那么就可以继续游戏，第一局免费。";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(44, 368);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(172, 15);
+            this.label16.TabIndex = 19;
+            this.label16.Text = "玩家可以随时停止游戏。";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(41, 434);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(97, 15);
+            this.label17.TabIndex = 20;
+            this.label17.Text = "玩了多少局：";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(165, 433);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(15, 15);
+            this.label18.TabIndex = 21;
+            this.label18.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 468);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 35);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "重置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 524);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.label1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ bestStopTime/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bestStopTime
+{
+    public partial class Form1 : Form
+    {
+        static double c = 0;//当前局收益
+        static double m = 0;//总收益
+        static double n = 0;//下一局的期望
+        static double g = 0;//玩一局的成本
+        static double p = 0;//折现率
+        static int count = 0;//玩了多少局
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            m = 0;
+            c = 0;
+            n = 0;
+            count = 0;
+
+            label7.Text = c.ToString();
+
+            label9.Text = m.ToString();
+
+            label10.Text = n.ToString();
+
+            label18.Text = count.ToString();
+
+          //  MessageBox.Show("重置成功！");
+        }
+
+        private void button6666_Click(object sender, EventArgs e)
+        {
+
+
+            if (textBox2.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("请填写每一局的成本");
+                return;
+            }
+            if (textBox1.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("请填写折现率");
+                return;
+            }
+            g = double.Parse(textBox2.Text.ToString());
+            p = double.Parse(textBox1.Text.ToString());
+            if (g < 0 || g > 1100)
+            {
+                MessageBox.Show("每一局的成本要在[0,1100)这个区间内！");
+                return;
+            }
+
+            if (p <= 0 || p > 1)
+            {
+                MessageBox.Show("折现率要在(0,1]这个区间内！");
+                return;
+            }
+
+            if (count > 0 && m < g)
+            {
+                MessageBox.Show("成本不足，无法继续游戏！");
+                return;
+            }
+
+            /*
+                static double c = 0;//当前局收益
+                static double m = 0;//总收益
+                static double n = 0;//下一局的期望
+                static double g = 0;//玩一局的成本
+                static double p = 0;//折现率
+                static bool next = true;//策略提示
+                static int count = 0;//玩了多少局
+             */
+            count++;//玩了多少局
+
+            Random rNumber = new Random();
+            int a = rNumber.Next(1, 13);
+
+            if (a == 12)
+            {
+
+                c = (-1) * m;
+
+                m = 0;
+
+                label7.Text = c.ToString();
+
+                label9.Text = "抽到12，总收益清零！！";
+
+                label10.Text = n.ToString();
+
+                label18.Text = count.ToString();
+
+                return;
+            }
+
+            c = a * 100;//当前局收益
+
+            if (count == 1)
+            {
+                m = m * p + c;//总收益，第一局不需要成本
+            }
+            else
+            {
+                m = m * p + c - g;//总收益
+            }
+
+
+            n = (-1) * m * (1 - p) - m / 12 + 550 - g;//下一局的期望
+
+            label7.Text = c.ToString();
+
+            label9.Text = m.ToString();
+
+            label10.Text = n.ToString();
+
+            label18.Text = count.ToString();
+
+            if (n >= 0)//策略提示
+            {
+                label11.Text = "扣除成本和折现，下一局期望为正，可以继续玩！";
+            }
+            else
+            {
+                label11.Text = "扣除成本和折现，下一局期望为负，建议停止游戏！";
+                MessageBox.Show("下一局期望为负，建议停止游戏！！！");
+            }
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
